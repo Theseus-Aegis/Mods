50be8f6ebce72473f9faf5b5f0fb00ab5e83e996
jonpas <jonpas33@gmail.com>
2015-06-25 20:55:49 +0200

    Added Interaction menu icon

--- addons/ratelmarker/CfgVehicles.hpp
+++ addons/ratelmarker/CfgVehicles.hpp
@@ -5,10 +5,10 @@ class CfgVehicles {
             class GVAR(RatelMarker) {
                 displayName = CSTRING(DisplayName);
                 condition = QUOTE(_this call FUNC(canUseMarkerMenu));
-                statement = QUOTE([] spawn FUNC(createMarkerMenu)); // createDisplay requires spawn
+                statement = QUOTE([] spawn FUNC(createMarkerMenu)); // createDisplay requires spawn for not-yet-known reasons
                 showDisabled = 0;
                 priority = 0;
-                //icon = QUOTE(PATHTOF(UI\ratelMarker_ca.paa));
+                icon = QUOTE(PATHTOF(UI\ratelMarker_ca.paa));
             };
         };
     };
