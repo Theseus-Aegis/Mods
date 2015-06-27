0377e164ecfb0316b066e52ab23d50a19f7332d1
jonpas <jonpas33@gmail.com>
2015-06-27 03:44:01 +0200

    Improved getRadioTracks and CfgSounds lenght via pre-processing, Restricted back to Helicopters only due to volume issues

--- addons/radios/CfgSounds.hpp
+++ addons/radios/CfgSounds.hpp
@@ -1,57 +1,21 @@
-#define distance 250
-#define volume db+15
+#define SOUND(var) \
+    class GVAR(var) { \
+        name = CSTRING(var); \
+        sound[] = {QUOTE(PATHTOF(sounds\var.ogg)), db+25, 1, 500}; \
+        titles[] = {}; \
+    };
 
 class CfgSounds {
     sounds[] = {track1, track2, track3, track4, track5, track6, track7, track8, track9, track10};
 
-    class GVAR(Track1) {
-        name = CSTRING(track1);
-        sound[] = {QUOTE(PATHTOF(sounds\track1.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track2) {
-        name = CSTRING(track2);
-        sound[] = {QUOTE(PATHTOF(sounds\track2.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track3) {
-        name = CSTRING(track3);
-        sound[] = {QUOTE(PATHTOF(sounds\track3.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track4) {
-        name = CSTRING(track4);
-        sound[] = {QUOTE(PATHTOF(sounds\track4.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track5) {
-        name = CSTRING(track5);
-        sound[] = {QUOTE(PATHTOF(sounds\track5.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track6) {
-        name = CSTRING(track6);
-        sound[] = {QUOTE(PATHTOF(sounds\track6.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track7) {
-        name = CSTRING(track7);
-        sound[] = {QUOTE(PATHTOF(sounds\track7.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track8) {
-        name = CSTRING(track8);
-        sound[] = {QUOTE(PATHTOF(sounds\track8.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track9) {
-        name = CSTRING(track9);
-        sound[] = {QUOTE(PATHTOF(sounds\track9.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
-    class GVAR(Track10) {
-        name = CSTRING(track10);
-        sound[] = {QUOTE(PATHTOF(sounds\track10.ogg)), volume, 1, distance};
-        titles[] = {};
-    };
+    SOUND(track1)
+    SOUND(track2)
+    SOUND(track3)
+    SOUND(track4)
+    SOUND(track5)
+    SOUND(track6)
+    SOUND(track7)
+    SOUND(track8)
+    SOUND(track9)
+    SOUND(track10)
 };
--- addons/radios/CfgVehicles.hpp
+++ addons/radios/CfgVehicles.hpp
@@ -1,41 +1,19 @@
-#define ICON_STOPRADIO QUOTE(PATHTOF(UI\musicStop_ca.paa))
-#define ICON_PLAYRADIO QUOTE(PATHTOF(UI\musicStart_ca.paa))
-
-#define MACRO_RADIOS \
-    class ACE_SelfActions { \
-        class GVAR(RadioStop) { \
-            displayName = CSTRING(StopRadio); \
-            condition = QUOTE(_this call FUNC(canStopRadio)); \
-            statement = QUOTE(_this call FUNC(stopRadio)); \
-            icon = ICON_STOPRADIO; \
-        }; \
-        class GVAR(RadioPlay) { \
-            displayName = CSTRING(PlayRadio); \
-            condition = QUOTE(_this call FUNC(canPlayRadio)); \
-            insertChildren = QUOTE(_this call DFUNC(addRadioTrackActions)); \
-            icon = ICON_PLAYRADIO; \
-        }; \
-    };
-
 class CfgVehicles {
-    class LandVehicle;
-    class Car: LandVehicle {
-        MACRO_RADIOS
-    };
-    class Tank: LandVehicle {
-        MACRO_RADIOS
-    };
-
     class Air;
     class Helicopter: Air {
-        MACRO_RADIOS
-    };
-    class Plane: Air {
-        MACRO_RADIOS
-    };
-
-    class Ship;
-    class Ship_F: Ship {
-        MACRO_RADIOS
+        class ACE_SelfActions {
+            class GVAR(RadioStop) {
+                displayName = CSTRING(StopRadio);
+                condition = QUOTE(_this call FUNC(canStopRadio));
+                statement = QUOTE(_this call FUNC(stopRadio));
+                icon = QUOTE(PATHTOF(UI\musicStop_ca.paa));
+            };
+            class GVAR(RadioPlay) {
+                displayName = CSTRING(PlayRadio);
+                condition = QUOTE(_this call FUNC(canPlayRadio));
+                insertChildren = QUOTE(_this call DFUNC(addRadioTrackActions));
+                icon = QUOTE(PATHTOF(UI\musicStart_ca.paa));
+            };
+        };
     };
 };
