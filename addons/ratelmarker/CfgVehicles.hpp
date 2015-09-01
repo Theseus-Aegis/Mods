1fe3c57deb2eb27cf060958b6d77690819736f9d
jonpas <jonpas33@gmail.com>
2015-09-01 20:44:54 +0200

    Removed spawn from create RATEL marker menu

--- addons/ratelmarker/CfgVehicles.hpp
+++ addons/ratelmarker/CfgVehicles.hpp
@@ -5,7 +5,7 @@ class CfgVehicles {
             class GVAR(RatelMarker) {
                 displayName = CSTRING(DisplayName);
                 condition = QUOTE(_this call FUNC(canUseMarkerMenu));
-                statement = QUOTE([] spawn FUNC(createMarkerMenu)); // createDisplay requires spawn for not-yet-known reasons
+                statement = QUOTE(call FUNC(createMarkerMenu)); // createDisplay requires spawn for not-yet-known reasons
                 showDisabled = 0;
                 priority = 0;
                 icon = QUOTE(PATHTOF(UI\ratelMarker_ca.paa));
