4404abcc3489cc6228bb53de0b6ee2d9d78fd1b1
jonpas <jonpas33@gmail.com>
2015-10-14 22:31:31 +0200

    CHange track interaction menu text to be parsed from config rather than stringtable

--- addons/radios/CfgSounds.hpp
+++ addons/radios/CfgSounds.hpp
@@ -1,21 +1,21 @@
-#define SOUND(var) \
-    class GVAR(var) { \
-        name = CSTRING(var); \
-        sound[] = {QUOTE(PATHTOF(sounds\var.ogg)), db+25, 1, 500}; \
+#define SOUND(var1,var2) \
+    class GVAR(var2) { \
+        name = var2; \
+        sound[] = {QUOTE(PATHTOF(sounds\var1.ogg)), db+25, 1, 500}; \
         titles[] = {}; \
     };
 
 class CfgSounds {
     sounds[] = {track1, track2, track3, track4, track5, track6, track7, track8, track9, track10};
 
-    SOUND(track1)
-    SOUND(track2)
-    SOUND(track3)
-    SOUND(track4)
-    SOUND(track5)
-    SOUND(track6)
-    SOUND(track7)
-    SOUND(track8)
-    SOUND(track9)
-    SOUND(track10)
+    SOUND(track1,Ride_of_the_Valkyries)
+    SOUND(track2,Hot_Nights_In_Los_Angeles)
+    SOUND(track3,Smokin_Camels)
+    SOUND(track4,Microvolt)
+    SOUND(track5,Silhouette)
+    SOUND(track6,Scorpion)
+    SOUND(track7,Lost_In_The_Shadows)
+    SOUND(track8,Flight)
+    SOUND(track9,Gravity)
+    SOUND(track10,Heat)
 };
