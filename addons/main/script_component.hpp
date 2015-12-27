51bb24bc7da78a5a3cbfe9c9399336c1fea6d47e
jonpas <jonpas33@gmail.com>
2015-12-27 15:42:33 +0100

    Fix macros inclusion path, Fix debug define in main

--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -1,12 +1,12 @@
 #define COMPONENT main
 #include "\x\tac\addons\main\script_mod.hpp"
 
-#ifdef DEBUG_ENABLED_CORE
+#ifdef DEBUG_ENABLED_MAIN
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_CORE
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_CORE
+#ifdef DEBUG_SETTINGS_MAIN
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MAIN
 #endif
 
 #include "\x\tac\addons\main\script_macros.hpp"
