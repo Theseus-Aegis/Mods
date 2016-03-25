fc159826261d61c3fb540d19ba8281d2e8b0272e
jonpas <jonpas33@gmail.com>
2016-03-25 21:50:42 +0100

    Bump version to 1.3.0

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -3,12 +3,12 @@
 #define PREFIX tac
 
 #define MAJOR 1
-#define MINOR 2
-#define PATCHLVL 1
-#define BUILD 1
+#define MINOR 3
+#define PATCHLVL 0
+#define BUILD 0
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.54
+#define REQUIRED_VERSION 1.56
--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
-name = "Theseus Inc. Mods 1.2.1";
+name = "Theseus Inc. Mods 1.3.0";
 picture = "logo_tac_ca.paa";
 actionName = "Website";
 action = "http://www.theseus-aegis.com/";
