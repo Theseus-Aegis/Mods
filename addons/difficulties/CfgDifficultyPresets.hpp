75892016e62d5e084a0ecd8179344da7c5099af2
jonpas <jonpas33@gmail.com>
2016-04-22 13:06:24 +0200

    Add Theseus difficulty

---
+++ addons/difficulties/CfgDifficultyPresets.hpp
@@ -0,0 +1,31 @@
+class CfgDifficultyPresets {
+    class Custom;
+    class GVAR(Theseus): Custom {
+        displayName = "Theseus";
+        description = "Custom difficulty for Theseus gameplay.";
+        optionDescription = "Custom difficulty for Theseus gameplay.";
+        levelAI = "AILevelHigh";
+        class Options {
+            autoReport = 0;
+            cameraShake = 1;
+            commands = 1;
+            deathMessages = 0;
+            detectedMines = 0;
+            enemyTags= 0;
+            friendlyTags = 0;
+            groupIndicators = 0;
+            mapContent = 0;
+            multipleSaves = 0;
+            reducedDamage = 0;
+            scoreTable = 0;
+            staminaBar = 0;
+            stanceIndicator = 1;
+            thirdPersonView = 0;
+            visionAid = 0;
+            vonID = 0;
+            waypoints = 0;
+            weaponCrosshair = 0;
+            weaponInfo = 1;
+        };
+    };
+};
