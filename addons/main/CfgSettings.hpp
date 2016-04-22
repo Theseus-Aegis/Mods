4fc68e0cad313b68505fcac25e9c75661e1ead29
jonpas <jonpas33@gmail.com>
2016-04-22 13:16:19 +0200

    Add CBA Versioning for ACE3, Move ace_zeus dependency to tac_zeus

---
+++ addons/main/CfgSettings.hpp
@@ -0,0 +1,11 @@
+class CfgSettings {
+    class CBA {
+        class Versioning {
+            class PREFIX {
+                class dependencies {
+                    ACE[] = {"ace_main", REQUIRED_ACE_VERSION, "(true)"};
+                };
+            };
+        };
+    };
+};
