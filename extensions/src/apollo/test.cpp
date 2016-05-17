dd4c707d5d2a52afae7169e0f0a0f0beaddec5da
jonpas <jonpas33@gmail.com>
2016-05-17 17:40:01 +0200

    Experimentation

--- extensions/src/apollo/test.cpp
+++ extensions/src/apollo/test.cpp
@@ -1,5 +1,5 @@
-
 #include "intercept.hpp"
+#include "uncategorized.hpp"
 
 int __cdecl intercept::api_version() {
     return 1;
@@ -15,7 +15,20 @@ void __cdecl intercept::fired(
     rv_string &magazine,
     object &projectile_)
 {
-    sqf::hint("weapon fired" + std::to_string(sqf::diag_frameno()));
+    sqf::hint("weapon fired " + std::to_string(sqf::diag_frameno()));
+    sqf::get_unit_loadout(sqf::player()); //crash
+    //sqf::rv_handgun_items items = sqf::handgun_items(unit_); //crash
+    //sqf::rv_model_info model_info = sqf::get_model_info(unit_);
+    //sqf::rv_fog_parameters fog = sqf::fog_params(); //works
+
+
+    /*std::vector<sqf::rv_magazine_ammo> mag_ammo = sqf::magazines_ammo(unit_);
+
+    std::vector<game_value> mag_ammo_gv;
+    for (sqf::rv_magazine_ammo ammo : mag_ammo)
+        mag_ammo_gv.push_back(game_value(ammo));*/
+
+    //sqf::diag_log(loadout);
 }
 
 
