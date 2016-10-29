52e20fc13874e31c187706ce14921de995987fa4
jonpas <jonpas33@gmail.com>
2016-10-29 19:46:27 +0200

    Set required CBA version to 3.1.1

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -12,5 +12,5 @@
 
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.62
-#define REQUIRED_CBA_VERSION {3,1,0}
+#define REQUIRED_CBA_VERSION {3,1,1}
 #define REQUIRED_ACE_VERSION {3,7,0}
