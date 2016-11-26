c140f2a7335e9099f5f6b0559bfe734ea2cdee19
jonpas <jonpas33@gmail.com>
2016-11-26 02:15:09 +0100

    Use EGVAR in compats

--- optionals/compat_cha_av8b/CfgVehicles.hpp
+++ optionals/compat_cha_av8b/CfgVehicles.hpp
@@ -2,6 +2,6 @@ class CfgVehicles {
 /* Air */
     class Plane;
     class Cha_AV8B2: Plane {
-        TAC_HeavyLifter_attachPos[] = {0, 0, 1.3};
+        EGVAR(heavyLifter,AttachPos[]) = {0, 0, 1.3};
     };
 };
