533b85b29701b801c46445d92dcba0521fea9e83
jonpas <jonpas33@gmail.com>
2016-12-29 19:35:20 +0100

    Add exported config

--- optionals/cup_units_nouniformrestrictions/README.md
+++ optionals/cup_units_nouniformrestrictions/README.md
@@ -5,3 +5,7 @@ Allows universal usage (no side restrictions) of uniforms from [CUP Units](http:
 ### Authors
 
 - [Jonpas](http://github.com/jonpas)
+
+### Notes
+
+- Generated using [ACE3 No Uniform Restrictions Export Config](https://github.com/acemod/ACE3/blob/master/optionals/nouniformrestrictions/functions/fnc_exportConfig.sqf)
--- optionals/cup_units_nouniformrestrictions/config.cpp
+++ optionals/cup_units_nouniformrestrictions/config.cpp
@@ -5,7 +5,23 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"tac_main", "cup_units"};
+        requiredAddons[] = {
+            "tac_main",
+            "CUP_Creatures_Military_ACR",
+            "CUP_Creatures_Military_BAF",
+            "CUP_Creatures_Military_CDF",
+            "CUP_Creatures_Military_Germany",
+            "CUP_Creatures_Military_Chedaki"
+            "CUP_Creatures_Military_NAPA",
+            "CUP_Creatures_Military_RACS",
+            "CUP_Creatures_Military_Russia",
+            "CUP_Creatures_Military_SLA",
+            "CUP_Creatures_Military_Taki",
+            "CUP_Creatures_Military_TakiInsurgents",
+            "CUP_Creatures_Military_USArmy",
+            "CUP_Creatures_Military_USMC",
+            "CUP_Creatures_People_Civil_Chernarus",
+        };
         author = ECSTRING(main,Author);
         authors[] = {"Jonpas"};
         url = ECSTRING(main,URL);
