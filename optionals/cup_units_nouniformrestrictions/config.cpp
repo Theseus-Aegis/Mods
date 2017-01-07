596c85ab0135312067398ba0bf4d5842e0c87a95
jonpas <jonpas33@gmail.com>
2017-01-07 20:52:52 +0100

    Fix requiredAddons again

--- optionals/cup_units_nouniformrestrictions/config.cpp
+++ optionals/cup_units_nouniformrestrictions/config.cpp
@@ -7,20 +7,21 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "tac_main",
+            "CUP_Creatures_People_Civil_Chernarus",
             "CUP_Creatures_Military_ACR",
             "CUP_Creatures_Military_BAF",
             "CUP_Creatures_Military_CDF",
-            "CUP_Creatures_Military_Germany",
             "CUP_Creatures_Military_Chedaki",
+            "CUP_Creatures_Military_Germany",
             "CUP_Creatures_Military_NAPA",
+            "CUP_Creatures_Military_PMC",
             "CUP_Creatures_Military_RACS",
             "CUP_Creatures_Military_Russia",
             "CUP_Creatures_Military_SLA",
             "CUP_Creatures_Military_Taki",
             "CUP_Creatures_Military_TakiInsurgents",
             "CUP_Creatures_Military_USArmy",
-            "CUP_Creatures_Military_USMC",
-            "CUP_Creatures_People_Civil_Chernarus"
+            "CUP_Creatures_Military_USMC"
         };
         author = ECSTRING(main,Author);
         authors[] = {"Jonpas"};
