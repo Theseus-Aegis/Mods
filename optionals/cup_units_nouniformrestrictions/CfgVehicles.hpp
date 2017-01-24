6a7293d914cf5df732d60f971fead02ebba77646
jonpas <jonpas@users.noreply.github.com>
2017-01-24 16:30:36 +0100

    Add optional CUP Units No Uniform Restrictions (#255)
    * Add CUP Units No Uniform Restrictions
    
    * Add exported config
    
    * Correct debug define names
    
    * Fix requiredAddons
    
    * Fix requiredAddons again

---
+++ optionals/cup_units_nouniformrestrictions/CfgVehicles.hpp
@@ -0,0 +1,55 @@
+class CfgVehicles {
+    class SoldierWB;
+
+    class CUP_Creatures_Military_GER_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_NAPA_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_RACS_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_RUS_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_SLA_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_TK_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_TKI_Insurgent_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_TKG_Guerrilla_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_USA_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_USMC_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_FR_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Civil_Chernarus_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_ACR_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_BAF_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_CDF_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_UNO_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+    class CUP_Creatures_Military_OPFINS_Soldier_Base: SoldierWB {
+        modelSides[] = {6};
+    };
+};
