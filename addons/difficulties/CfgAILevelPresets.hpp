af6b58cb7ff372c29b4076d8d3a29672273712b5
jonpas <jonpas@users.noreply.github.com>
2017-07-18 04:00:24 +0200

    Add Theseus AI difficulty (#298)
    * Add Theseus AI difficulty
    
    * Fix missing quotes
    
    * Incorporate pre-ACE 3.10.0 CfgAISkill changes, Increase global precisionAI to 0.4

---
+++ addons/difficulties/CfgAILevelPresets.hpp
@@ -0,0 +1,8 @@
+class CfgAILevelPresets {
+    class Custom;
+    class GVAR(Theseus): Custom {
+        displayName = "Theseus";
+        precisionAI = 0.4;
+        skillAI = 0.8;
+    };
+};
---
+++ addons/difficulties/CfgAISkill.hpp
@@ -0,0 +1,21 @@
+/*
+ * Incorporated from pre-ACE 3.10.0
+ *
+ * The idea is to reduce the AI's godlike aiming capabilties while retaining its high intelligence.
+ * The AI should be smart enough to move through a town, but also be "human" in their reaction time and aim.
+ *
+ * Note: All these values can still be adjusted via scripts within interpolation boundaries.
+ */
+
+class CfgAISkill {
+    aimingAccuracy[] = {0, 0, 1, 0.8};
+    aimingShake[] = {0, 0, 1, 0.6};
+    aimingSpeed[] = {0, 0, 1, 0.7};
+    commanding[] = {0, 0, 1, 0.8};
+    courage[] = {0, 0, 1, 0.7};
+    endurance[] = {0, 0, 1, 0.7};
+    general[] = {0, 0, 1, 0.9};
+    //reloadSpeed[] changes break rapid firing in single fire mode for players
+    spotDistance[] = {0, 0, 1, 0.9};
+    spotTime[] = {0, 0, 1, 0.7};
+};
