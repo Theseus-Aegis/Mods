b7b342ecf8fad205b11f83131b9b73b3bd52ffd1
jonpas <jonpas33@gmail.com>
2017-12-20 17:40:47 +0100

    Prepare release 1.14.0

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 1
-#define MINOR 13
-#define PATCHLVL 1
-#define BUILD 31
+#define MINOR 14
+#define PATCHLVL 0
+#define BUILD 32
--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
-name = "Theseus Inc. Mods 1.13.1";
+name = "Theseus Inc. Mods 1.14.0";
 picture = "logo_tac_ca.paa";
 actionName = "Website";
 action = "http://www.theseus-aegis.com/";
