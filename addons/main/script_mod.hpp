6fb869dbe7d9ff6436d496dcc0434d49d6dab6f2
jonpas <jonpas33@gmail.com>
2018-04-23 20:19:54 +0200

    Require Arma 3 v1.82 and CBA 3.7.0

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -8,8 +8,8 @@
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.78
-#define REQUIRED_CBA_VERSION {3,5,0}
+#define REQUIRED_VERSION 1.82
+#define REQUIRED_CBA_VERSION {3,7,0}
 #define REQUIRED_ACE_VERSION {3,12,2}
 
 #ifdef COMPONENT_BEAUTIFIED
