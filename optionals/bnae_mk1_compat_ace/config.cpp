27476264c9b6c28349d78b282553086b85043d39
Josuan Albin <josuanalbin@outlook.fr>
2018-12-21 15:47:08 +0100

    Fix mk1 compat styling

--- optionals/bnae_mk1_compat_ace/config.cpp
+++ optionals/bnae_mk1_compat_ace/config.cpp
@@ -1,59 +1,55 @@
-class CfgPatches
-{
-	class bnae_mk1_compat_ace
-	{
-		requiredaddons[]=
-		{
-			"A3_Weapons_F",
-			"bnae_core",
-			"bnae_mk1"
-		};
-		requiredversion=0.1;
-		weapons[]=
-		{
-			"bnae_mk1_virtual",
-			"bnae_mk1_t_virtual",
-			"bnae_mk1_t_camo1_virtual",
-			"bnae_mk1_no4_t_virtual"
-		};
-		units[]=
-		{
-			"bnae_mk1_editor"
-		};
-	};
+class CfgPatches {
+    class bnae_mk1_compat_ace {
+        requiredaddons[]= {
+            "A3_Weapons_F",
+            "bnae_core",
+            "bnae_mk1"
+        };
+        requiredversion=0.1;
+        weapons[]= {
+            "bnae_mk1_virtual",
+            "bnae_mk1_t_virtual",
+            "bnae_mk1_t_camo1_virtual",
+            "bnae_mk1_no4_t_virtual"
+        };
+       units[]= {
+            "bnae_mk1_editor"
+        };
+    };
 };
+
 class Mode_SemiAuto;
-class CfgWeapons
-{
-	class Rifle_Long_Base_F;
-	class bnae_mk1_base: Rifle_Long_Base_F
-	{
+
+class CfgWeapons {
+    class Rifle_Long_Base_F;
+
+    class bnae_mk1_base: Rifle_Long_Base_F {
         initSpeed=-1.0;
-		ACE_barrelTwist=254.0;
-		ACE_barrelLength=640.08;
-		ACE_RailHeightAboveBore=1.8;
-		class Single: Mode_SemiAuto
-		{
-			dispersion=0.00044;
-		};
-	};
+        ACE_barrelTwist=254.0;
+        ACE_barrelLength=640.08;
+        ACE_RailHeightAboveBore=1.8;
+        class Single: Mode_SemiAuto {
+            dispersion=0.00044;
+        };
+    };
 };
-class CfgMagazines
-{
-	class CA_Magazine;
-    class 10Rnd_303_Magazine: CA_Magazine
-	{
-		ammo="B_303_Ball";
-		initSpeed=763; // muzzle velocity according with the barrel length
-	};
+
+class CfgMagazines {
+    class CA_Magazine;
+
+    class 10Rnd_303_Magazine: CA_Magazine {
+       ammo="B_303_Ball";
+       initSpeed=763; // muzzle velocity according with the barrel length
+    };
 };
-class CfgAmmo
-{
-	class BulletBase;
-    class B_303_Ball: BulletBase // class ACE_303_Ball; https://github.com/acemod/ACE3/blob/e5a15d200f44df5fccc0bc5575d18d80b35538dd/extras/CfgAmmoReference.hpp#L412
-	{
-		typicalSpeed=763; // muzzle velocity according with the barrel length
-		airFriction=-0.00083;
+
+class CfgAmmo {
+    class BulletBase;
+
+    // class ACE_303_Ball; https://github.com/acemod/ACE3/blob/e5a15d200f44df5fccc0bc5575d18d80b35538dd/extras/CfgAmmoReference.hpp#L412
+    class B_303_Ball: BulletBase {
+        typicalSpeed=763; // muzzle velocity according with the barrel length
+        airFriction=-0.00083;
         ACE_caliber=7.899;
         ACE_bulletLength=31.166;
         ACE_bulletMass=11.2752;
@@ -64,5 +60,5 @@ class CfgAmmo
         ACE_dragModel=1;
         ACE_muzzleVelocities[]={763}; // muzzle velocity according with the barrel length
         ACE_barrelLengths[]={640.08};
-	};
+    };
 };
