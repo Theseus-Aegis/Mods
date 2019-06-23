93f4e411cddb989569ae5e5552787f5622ffdaa6
Josuan Albin <josuanalbin@outlook.fr>
2019-06-24 00:02:08 +0200

    Fix painkillers for medical rewrite

--- addons/medical/ACE_Medical_Actions.hpp
+++ addons/medical/ACE_Medical_Actions.hpp
@@ -1,34 +1,24 @@
-class ACE_Medical_Actions {
-    class Basic {
-        class Morphine;
-        class GVAR(painkillers): Morphine {
-            displayName = CSTRING(DisplayName);
-            displayNameProgress = CSTRING(PainkillerProgress);
-            allowedSelections[] = {"head"};
-            items[] = {QGVAR(painkillers)};
-        };
-    };
-    class Advanced {
-        class Morphine;
-        class GVAR(painkillers): Morphine {
-            displayName = CSTRING(DisplayName);
-            displayNameProgress = CSTRING(PainkillerProgress);
-            allowedSelections[] = {"head"};
-            items[] = {QGVAR(painkillers)};
-        };
+class ace_medical_treatment_actions {
+    class Morphine;
+    class GVAR(painkillers): Morphine {
+        displayName = CSTRING(DisplayName);
+        displayNameProgress = CSTRING(PainkillerProgress);
+        allowedSelections[] = {"head"};
+        items[] = {QGVAR(painkillers)};
+        icon = QPATHTOF(ui\painkillers_icon_ca.paa);
+        condition = "";
+        callbackSuccess = 'call ace_medical_treatment_fnc_medication';
     };
 };
 
-class ACE_Medical_Advanced {
-    class Treatment {
-        class Medication {
-            class GVAR(painkillers) {
-                painReduce = 0.7;
-                timeInSystem = 400;
-                maxDose = 4;
-                inCompatableMedication[] = {};
-                viscosityChange = 5;
-            };
+class ace_medical_treatment {
+    class Medication {
+        class GVAR(painkillers) {
+            painReduce = 0.7;
+            timeInSystem = 400;
+            maxDose = 4;
+            inCompatableMedication[] = {};
+            viscosityChange = 5;
         };
     };
 };
