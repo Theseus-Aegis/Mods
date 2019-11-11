652148d717ffd739da1d1822d6d76467d5c1c74e
Josuan Albin <alganthe@live.fr>
2019-11-12 00:14:23 +0100

    Update painkillers for ACE3 Medical Rewrite (#393)
    * Adjust painkillers for medical rewrite

--- addons/medical/CfgVehicles.hpp
+++ addons/medical/CfgVehicles.hpp
@@ -11,35 +11,4 @@ class CfgVehicles {
             MACRO_ADDITEM(GVAR(painkillers),1);
         };
     };
-
-    class Man;
-    class CAManBase: Man {
-        class ACE_SelfActions {
-            class Medical {
-                class ACE_Head {
-                    class FieldDressing;
-                    class GVAR(painkillers): FieldDressing {
-                        displayName = CSTRING(PainkillersSelfActionDisplayName);
-                        condition = '[_player, _target, "head", QGVAR(painkillers)] call ACEFUNC(medical,canTreatCached)';
-                        exceptions[] = {"isNotInside", "isNotSwimming"};
-                        statement = '[_player, _target, "head", QGVAR(painkillers)] call ACEFUNC(medical,treatment)';
-                        icon = QPATHTOF(ui\painkillers_icon_ca.paa);
-                    };
-                };
-            };
-        };
-
-        class ACE_Actions {
-            class ACE_Head {
-                class FieldDressing;
-                class GVAR(painkillers): FieldDressing {
-                    displayName = CSTRING(PainkillersActionDisplayName);
-                    condition = '[_player, _target, "head", QGVAR(painkillers)] call ACEFUNC(medical,canTreatCached)';
-                    exceptions[] = {"isNotInside", "isNotSwimming"};
-                    statement = '[_player, _target, "head", QGVAR(painkillers)] call ACEFUNC(medical,treatment)';
-                    icon = QPATHTOF(ui\painkillers_icon_ca.paa);
-                };
-            };
-        };
-    };
 };
