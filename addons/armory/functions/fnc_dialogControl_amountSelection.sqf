Theseus-Aegis/Mods

commit 4ea6f72ce4c0ea862937ad69754aea5083559e55

Default to max item stash quantity in Armory (#410)

addons/armory/functions/fnc_dialogControl_amountSelection.sqf
@@ -16,15 +16,16 @@
  */
 
 private _quantity = CTRL(NLIST) lnbData [lnbCurSelRow NLIST, 2];
+_quantity = parseNumber _quantity;
 TRACE_1("Amount of selected item",_quantity);
 
 lbClear DROPDOWNAMOUNT;
 
-for "_x" from 1 to (parseNumber _quantity) do {
+for "_x" from 1 to _quantity do {
     lbAdd [DROPDOWNAMOUNT, str _x];
 };
 
-// Set initial value to 1 (will not fire onLBSelChanged)
-lbSetCurSel [DROPDOWNAMOUNT, 0];
+// Set initial value to max/all (will not fire onLBSelChanged)
+lbSetCurSel [DROPDOWNAMOUNT, _quantity];
 
 ctrlShow [DROPDOWNAMOUNT, true];

commit 723176a8b3ebfcda2bbe3dd43a447160900876f1

Default to 1 item take quantity in Armory
Regression from #410

addons/armory/functions/fnc_dialogControl_amountSelection.sqf
@@ -26,6 +26,6 @@ for "_x" from 1 to _quantity do {
 };
 
 // Set initial value to max/all (will not fire onLBSelChanged)
-lbSetCurSel [DROPDOWNAMOUNT, _quantity];
+lbSetCurSel [DROPDOWNAMOUNT, [1, _quantity] select (GVAR(selectedCategory) == "stash")];
 
 ctrlShow [DROPDOWNAMOUNT, true];