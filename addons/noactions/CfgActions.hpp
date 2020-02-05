d1f41c0cf1326eee870c5a14c15fa892e0c46a30
jonpas <jonpas33@gmail.com>
2020-02-05 18:30:09 +0100

    No Actions - Fix `LightOff` inheritance (#425)

--- addons/noactions/CfgActions.hpp
+++ addons/noactions/CfgActions.hpp
@@ -1,5 +1,5 @@
-#define NO_ACTION_CLASS(NAME, DEFAULT) \
-class NAME: None { \
+#define NO_ACTION_CLASS(NAME, PARENT, DEFAULT) \
+class NAME: PARENT { \
     show = QUOTE(call compile getText (configFile >> 'CfgActions' >> 'NAME' >> 'GVAR(setting)')); \
     GVAR(setting) = QUOTE(profileNamespace getVariable [ARR_2('GVAR(NAME)', DEFAULT)]); \
 } \
@@ -7,8 +7,8 @@ class NAME: None { \
 class CfgActions {
     class None;
 
-    NO_ACTION_CLASS(TurnIn,1);
-    NO_ACTION_CLASS(TurnOut,1);
-    NO_ACTION_CLASS(LightOn,1);
-    NO_ACTION_CLASS(LightOff,1);
+    NO_ACTION_CLASS(TurnIn,None,1);
+    NO_ACTION_CLASS(TurnOut,None,1);
+    NO_ACTION_CLASS(LightOn,None,1);
+    NO_ACTION_CLASS(LightOff,LightOn,1);
 };
