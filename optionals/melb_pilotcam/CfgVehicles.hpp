15d725d7b658a04a350857a2d17099f99a34ac61
jonpas <jonpas33@gmail.com>
2020-04-06 02:27:20 +0200

    Cleanup

--- optionals/melb_pilotcam/CfgVehicles.hpp
+++ optionals/melb_pilotcam/CfgVehicles.hpp
@@ -1,9 +1,9 @@
-class RscInGameUI {
+/*class RscInGameUI {
     class RscUnitInfo;
     class Rsc_MELB_Turret_UnitInfo: RscUnitInfo {
         onLoad = "['onLoad', _this, 'RscUnitInfo', 'IGUI'] call (uiNamespace getVariable 'BIS_fnc_initDisplay'); _this spawn tac_melb_pilotcam_fnc_handleFlir";
     };
-};
+};*/
 
 class CfgVehicles {
     class Helicopter_Base_H;
@@ -13,65 +13,60 @@ class CfgVehicles {
                 class Wide {
                     opticsDisplayName = "----";
                     initAngleX = 0;
-                    minAngleX = 0;
-                    maxAngleX = 0;
+                    minAngleX = -360;
+                    maxAngleX = 360;
                     initAngleY = 0;
-                    minAngleY = 0;
-                    maxAngleY = 0;
-                    initFov = 0.30000001;
-                    minFov = 0.30000001;
-                    maxFov = 0.30000001;
+                    minAngleY = -15;
+                    maxAngleY = 85;
+                    initFov = 0.3;
+                    minFov = 0.3;
+                    maxFov = 0.3;
                     visionMode[] = {"Normal", "NVG", "Ti"};
                     thermalMode[] = {0};
+                    directionStabilized = 1;
+                    horizontallyStabilized = 1;
                     gunnerOpticsModel = "melb\data\optics\melb_flir_wf.p3d";
-                    opticsPPEffects[] = {"OpticsCHAbera2", "OpticsBlur2"};
+                    opticsPPEffects[] = {"OpticsCHAbera3", "OpticsBlur3"};
                     gunnerOpticsEffect[] = {"TankCommanderOptics2"};
-                    //directionStabilized = 1;
                 };
-                class WideT2: Wide {
+                class WideT: Wide {
                     initFov = 0.2;
                     minFov = 0.2;
                     maxFov = 0.2;
                     gunnerOpticsModel = "melb\data\optics\melb_flir_w2.p3d";
                 };
-                class MediumT: Wide {
+                class MediumT: WideT {
                     initFov = 0.1;
                     minFov = 0.1;
                     maxFov = 0.1;
                     gunnerOpticsModel = "melb\data\optics\melb_flir_m.p3d";
                 };
-                class NarrowT: Wide {
+                class NarrowT: WideT {
                     initFov = 0.022;
                     minFov = 0.022;
                     maxFov = 0.022;
                     gunnerOpticsModel = "melb\data\optics\melb_flir_n.p3d";
                 };
-                class NarrowT2: Wide {
-                    initFov = 0.0092000002;
-                    minFov = 0.0092000002;
-                    maxFov = 0.0092000002;
+                class NarrowT2: WideT {
+                    initFov = 0.0092;
+                    minFov = 0.0092;
+                    maxFov = 0.0092;
                     gunnerOpticsModel = "melb\data\optics\melb_flir_n2.p3d";
                 };
-
-                showMiniMapInOptics = 1;
-                showUAVViewInOptics = 0;
-                showSlingLoadManagerInOptics = 0;
             };
 
-            initAngleX = 0;
-            minAngleX = -30;
-            maxAngleX = 30;
-            initAngleY = 0;
-            minAngleY = -100;
-            maxAngleY = 100;
-            initFov = 1.1;
-            minFov = 0.133;
-            maxFov = 1.1;
-            maxXRotSpeed = 0.30000001;
-            maxYRotSpeed = 0.30000001;
-            pilotOpticsShowCursor = 0;
+            minTurn = -180;
+            maxTurn = 180;
+            initTUrn = 0;
+            minElev = -180;
+            maxElev = 180;
+            initElev = 0;
+            maxXRotSpeed = 1;
+            maxYRotSpeed = 1;
+            maxMouseXRotSpeed = 0.5;
+            maxMouseYRotSpeed = 0.5;
+            pilotOpticsShowCursor = 1;
             controllable = 1;
-            turretInfoType = "Rsc_MELB_Turret_UnitInfo";
         };
         memoryPointDriverOptics = "commanderview";
     };
