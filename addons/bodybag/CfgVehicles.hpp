39c6057bd1e42be742623b53c566d8fa4159f379
TyroneMF <TyroneMF@hotmail.com>
2020-05-17 12:29:30 +0100

    Make Bodybags Carryable (#442)

--- addons/bodybag/CfgVehicles.hpp
+++ addons/bodybag/CfgVehicles.hpp
@@ -5,5 +5,8 @@ class CfgVehicles {
         transportMaxWeapons = 1;
         transportMaxMagazines = 1;
         transportMaxItems = 1;
+        ace_dragging_canCarry = 1; // 1 = enabled (0 disabled)
+        ace_dragging_carryPosition[] = {0, 0.5, 1.2};  // XYZ offset, 0.5m ahead, 1.2m up.
+        ace_dragging_carryDirection = 90; // Direction
     };
 };
