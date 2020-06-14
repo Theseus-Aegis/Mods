57624971d9d69bffae4ede2651a3d1351c3ab3d6
TyroneMF <TyroneMF@hotmail.com>
2020-06-14 15:49:33 +0100

    Remove Radios Component (#448)

--- addons/radios/XEH_PREP.hpp
+++
@@ -1,7 +0,0 @@
-PREP(addRadioTrackActions);
-PREP(canPlayRadio);
-PREP(canStopRadio);
-PREP(changeVolume);
-PREP(getRadioTracks);
-PREP(playRadio);
-PREP(stopRadio);
--- addons/radios/XEH_postInitClient.sqf
+++
@@ -1,5 +0,0 @@
-#include "script_component.hpp"
-
-[QGVAR(play), {
-    (_this select 0) say3D (_this select 1);
-}] call CBA_fnc_addEventHandler;
