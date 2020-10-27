b558188639ec4e7b5d3b467b0dfa9aea4b366f2f
TyroneMF <TyroneMF@hotmail.com>
2020-10-27 17:50:35 +0000

    Rename to Kunduz_Gridlines

--- optionals/kunduz_gridlines/script_component.hpp
+++ optionals/kunduz_gridlines/script_component.hpp
@@ -1,17 +1,17 @@
-#define COMPONENT compat_kunduz
-#define COMPONENT_BEAUTIFIED Kunduz Compatibility
+#define COMPONENT kunduz_gridlines
+#define COMPONENT_BEAUTIFIED Kunduz Gridlines
 #include "\x\tac\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#ifdef DEBUG_ENABLED_COMPAT_KUNDUZ
+#ifdef DEBUG_ENABLED_KUNDUZ_GRIDLINES
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_COMPAT_KUNDUZ
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_COMPAT_KUNDUZ
+#ifdef DEBUG_SETTINGS_KUNDUZ_GRIDLINES
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_KUNDUZ_GRIDLINES
 #endif
 
 #include "\x\tac\addons\main\script_macros.hpp"
