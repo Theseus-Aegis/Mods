e341c2d21fd8b15a690a9f4f5256d664c6b57c31
TyroneMF <TyroneMF@hotmail.com>
2020-10-27 10:22:22 +0000

    Add Gridlines to FRL Kunduz
    - title

---
+++ optionals/compat_kunduz/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT compat_kunduz
+#define COMPONENT_BEAUTIFIED Kunduz Compatibility
+#include "\x\tac\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_COMPAT_KUNDUZ
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_COMPAT_KUNDUZ
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_COMPAT_KUNDUZ
+#endif
+
+#include "\x\tac\addons\main\script_macros.hpp"
