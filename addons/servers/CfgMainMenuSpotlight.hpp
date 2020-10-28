254e95811d095462520102775b8bfc946eee1966
JoramD0 <j.davids@hotmail.nl>
2020-10-28 23:46:01 +0100

    It works!!!

--- addons/servers/CfgMainMenuSpotlight.hpp
+++ addons/servers/CfgMainMenuSpotlight.hpp
@@ -3,14 +3,14 @@ class CfgMainMenuSpotlight
     class JoinContractServer {
         text = CSTRING(contractsText);
         picture = QPATHTOF(ui\spotlight_theseus.paa);
-        action = '["213.239.214.39", 2302] call (uiNamespace getVariable FUNC(joinServer))';
+        action = '["213.239.214.39", 2302] call (uiNamespace getVariable QFUNC(joinServer))';
         actionText = CSTRING(contractsActionText);
         condition = "true";
     };
     class JoinTrainingServer {
         text = CSTRING(trainingText);
         picture = QPATHTOF(ui\spotlight_theseus.paa);
-        action = '["213.239.214.39", 2502] call (uiNamespace getVariable FUNC(joinServer))';
+        action = '["213.239.214.39", 2502] call (uiNamespace getVariable QFUNC(joinServer))';
         actionText = CSTRING(trainingActionText);
         condition = "true";
     };
