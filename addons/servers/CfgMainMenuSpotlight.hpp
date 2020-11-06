cc8caed5dbe212ee928b29a80ba2284903e411ad
JoramD <j.davids@hotmail.nl>
2020-11-06 11:00:53 +0100

    Add servers component (#462)
    Co-authored-by: jonpas <jonpas33@gmail.com>

---
+++ addons/servers/CfgMainMenuSpotlight.hpp
@@ -0,0 +1,41 @@
+class CfgMainMenuSpotlight {
+    class GVAR(joinContracts) {
+        text = CSTRING(contractsText);
+        picture = QPATHTOF(ui\spotlight_theseus.paa);
+        action = '["213.239.214.39", 2302] call (uiNamespace getVariable QFUNC(joinServer))';
+        actionText = CSTRING(contractsActionText);
+        condition = "true";
+    };
+    class GVAR(joinTraining) {
+        text = CSTRING(trainingText);
+        picture = QPATHTOF(ui\spotlight_theseus.paa);
+        action = '["213.239.214.39", 2502] call (uiNamespace getVariable QFUNC(joinServer))';
+        actionText = CSTRING(trainingActionText);
+        condition = "true";
+    };
+
+    delete ApexProtocol;
+    delete BootCamp;
+    delete EastWind;
+
+    delete Orange_CampaignGerman;
+    delete Orange_Showcase_IDAP;
+    delete Orange_Showcase_LoW;
+    delete Orange_Campaign;
+
+    delete Showcase_TankDestroyers;
+
+    delete Tacops_Campaign_03;
+    delete Tacops_Campaign_02;
+    delete Tacops_Campaign_01;
+
+    delete Tanks_Campaign_01;
+
+    delete OldMan;
+
+    delete Contact_Campaign;
+
+    delete gm_campaign_01;
+
+    delete SP_FD14;
+};
---
+++ addons/servers/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT servers
+#define COMPONENT_BEAUTIFIED Servers
+#include "\x\tac\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_SERVERS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SERVERS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SERVERS
+#endif
+
+#include "\x\tac\addons\main\script_macros.hpp"
