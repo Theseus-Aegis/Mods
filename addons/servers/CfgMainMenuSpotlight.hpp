a1c20e4185f723f7f8fdfd67cfa327d2297af46f
JoramD0 <j.davids@hotmail.nl>
2020-11-06 10:12:04 +0100

    Remove default mainMenuSpotlights

--- addons/servers/CfgMainMenuSpotlight.hpp
+++ addons/servers/CfgMainMenuSpotlight.hpp
@@ -13,4 +13,29 @@ class CfgMainMenuSpotlight {
         actionText = CSTRING(trainingActionText);
         condition = "true";
     };
+
+    delete ApexProtocol;
+    delete BootCamp;
+    delete EastWind;
+
+    delete Orange_CampaignGerman;
+    delete Orange_Showcase_IDAP;
+    delete Orange_Showcase_LoW;
+    delete Orange_Campaign;
+
+    delete Showcase_TankDestroyers;
+
+    delete Tacops_Campaign_03;
+    delete Tacops_Campaign_02;
+    delete Tacops_Campaign_01;
+
+    delete Tanks_Campaign_01;
+
+    delete OldMan;
+
+    delete Contact_Campaign;
+
+    delete gm_campaign_01;
+
+    delete SP_FD14;
 };
