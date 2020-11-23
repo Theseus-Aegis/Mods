d4fa49f6296aa2c7738a147ebb0b192d0a36c16b
JoramD <j.davids@hotmail.nl>
2020-11-23 10:51:41 +0100

    Update spotlight images (#470)

--- addons/servers/CfgMainMenuSpotlight.hpp
+++ addons/servers/CfgMainMenuSpotlight.hpp
@@ -1,14 +1,14 @@
 class CfgMainMenuSpotlight {
     class GVAR(joinContracts) {
         text = CSTRING(contractsText);
-        picture = QPATHTOF(ui\spotlight_theseus.paa);
+        picture = QPATHTOF(ui\spotlight_contracts.paa);
         action = '["213.239.214.39", 2302] call (uiNamespace getVariable QFUNC(joinServer))';
         actionText = CSTRING(contractsActionText);
         condition = "true";
     };
     class GVAR(joinTraining) {
         text = CSTRING(trainingText);
-        picture = QPATHTOF(ui\spotlight_theseus.paa);
+        picture = QPATHTOF(ui\spotlight_olympus.paa);
         action = '["213.239.214.39", 2502] call (uiNamespace getVariable QFUNC(joinServer))';
         actionText = CSTRING(trainingActionText);
         condition = "true";
