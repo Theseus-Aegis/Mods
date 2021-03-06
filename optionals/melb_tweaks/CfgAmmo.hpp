e51cfec790415ffd02cb7706d2c8df28057c1d37
jonpas <jonpas33@gmail.com>
2021-03-06 18:34:46 +0100

    MELB Tweaks - Match tracers closer to vanilla AH-9

--- optionals/melb_tweaks/CfgAmmo.hpp
+++ optionals/melb_tweaks/CfgAmmo.hpp
@@ -10,15 +10,16 @@ class CfgAmmo {
     class B_762x51_Ball;
     class B_762x51_Ball_MELB: B_762x51_Ball {
         // Revert to vanilla
-        tracerScale = 0.6;
-        tracerStartTime = 0.0075;
-        tracerEndTime = 5;
+        model = "\A3\Weapons_f\Data\bullettracer\tracer_white";
+        tracerScale = 1.2;
+        tracerStartTime = 0.073;
+        tracerEndTime = 2.15957;
     };
 
     class B_127x99_SLAP_Tracer_Red;
     class B_127x99_SLAP_MELB: B_127x99_SLAP_Tracer_Red {
         // Revert to vanilla
-        tracerScale = 1.2;
+        tracerScale = 1.3;
         tracerStartTime = 0.075;
         tracerEndTime = 1;
     };
