b97fbdad4f968bcadcc046472f4c3996a3a7c21d
TyroneMF <TyroneMF@hotmail.com>
2021-03-06 17:36:08 +0000

    initial push

--- optionals/melb_tweaks/CfgWeapons.hpp
+++ optionals/melb_tweaks/CfgWeapons.hpp
@@ -6,6 +6,7 @@ class CfgWeapons {
             class StandardSound {
                 begin1[] = {"A3\Sounds_F\arsenal\weapons_vehicles\gatling_762mm\762mm_01_burst", 3.98107 , 1, 1300 , {2, 36879}};
                 soundBegin[] = {"begin1", 1};
+                soundClosure[] = {"closure1", 1};
             };
         };
     };
