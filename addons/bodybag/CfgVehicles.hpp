3fda00a70512a22dc1e0d42361af5e6caceeff32
TyroneMF <TyroneMF@hotmail.com>
2021-10-05 11:49:38 +0100

    Update to APL-SA

--- addons/bodybag/CfgVehicles.hpp
+++ addons/bodybag/CfgVehicles.hpp
@@ -8,6 +8,6 @@ class CfgVehicles {
         ace_dragging_canCarry = 1; // 1 = enabled (0 disabled)
         ace_dragging_carryPosition[] = {0, 0.5, 1.2};  // XYZ offset, 0.5m ahead, 1.2m up.
         ace_dragging_carryDirection = 90; // Direction
-        hiddenSelectionsTextures[] = {QPATHTOF(apl\bodybag_co.paa)};
+        hiddenSelectionsTextures[] = {QPATHTOF(apl-sa\bodybag_co.paa)};
     };
 };
