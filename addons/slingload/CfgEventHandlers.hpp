9b291f3e8e244d3f888aca083e0aefb9d194af1f
jonpas <jonpas33@gmail.com>
2021-11-16 12:34:23 +0100

    Use COMPILE_SCRIPT

--- addons/slingload/CfgEventHandlers.hpp
+++ addons/slingload/CfgEventHandlers.hpp
@@ -1,5 +1,5 @@
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        serverInit = QUOTE(call COMPILE_FILE(XEH_postInitServer));
+        serverInit = QUOTE(call COMPILE_SCRIPT(XEH_postInitServer));
     };
 };
