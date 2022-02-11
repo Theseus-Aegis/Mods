502ebbcfb019b51aaec62a37c3e5df5181cd8659
jonpas <jonpas33@gmail.com>
2022-02-11 16:00:14 +0100

    Update to arma-rs 1.5

--- extensions/apollo_client/src/lib.rs
+++ extensions/apollo_client/src/lib.rs
@@ -1,4 +1,4 @@
-use arma_rs::{rv, rv_handler};
+use arma_rs::{arma, Extension};
 
 const REST_ENDPOINT: &str = "https://theseus-aegis.com:8443/apollo";
 
@@ -19,30 +19,33 @@ fn request(api: String) -> String {
     response
 }
 
-#[rv]
+#[arma]
+fn init() -> Extension {
+    Extension::build()
+        .command("load_armory", load_armory)
+        .command("load_player", load_player)
+        .command("get_accessible_item_classes", get_accessible_item_classes)
+        .command("get_training_identifiers", get_training_identifiers)
+        .command("version", version)
+        .finish()
+}
+
 pub fn load_armory(category: String, player_id: String, debug: bool) -> String {
     request(url!("armory", category, player_id, debug))
 }
 
-#[rv]
 pub fn load_player(player_id: String, debug: bool) -> String {
     request(url!("loadInfantry", player_id, debug))
 }
 
-#[rv]
 pub fn get_accessible_item_classes(player_id: String) -> String {
     request(url!("getAccessibleItemClasses", player_id))
 }
 
-#[rv]
 pub fn get_training_identifiers(player_id: String) -> String {
     request(url!("getTrainingIdentifiers", player_id))
 }
 
-#[rv]
 pub fn version() -> &'static str {
     env!("CARGO_PKG_VERSION")
 }
-
-#[rv_handler]
-fn init() {}
