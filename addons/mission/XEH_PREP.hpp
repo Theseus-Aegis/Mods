720436a78c1c9c13c46dd978661c538b69c54783
Mike-MF <TyroneMF@hotmail.com>
2022-10-19 16:07:11 +0100

    Separate functions into Base & Extra

--- addons/mission/XEH_PREP.hpp
+++ addons/mission/XEH_PREP.hpp
@@ -1,16 +1,21 @@
-PREP(baseSpectator);
+// Base
+PREP(aar)
+PREP(baseSpectator)
+PREP(collectIntel)
+PREP(godMode)
+PREP(hunt)
+PREP(reinforcements)
+PREP(unconscious)
+
+// Extra
 PREP(bomber);
 PREP(caves);
-PREP(collectIntel);
 PREP(connectBatteryToDefusable);
 PREP(d30Strike);
 PREP(forceShooting);
-PREP(godMode);
-PREP(hunt);
 PREP(mortarStrike);
-PREP(reinforcements);
+PREP(reaction)
 PREP(respiratorEffects);
 PREP(switchAction);
 PREP(teleport);
 PREP(toggleLights);
-PREP(unconscious);
