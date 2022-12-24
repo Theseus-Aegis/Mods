2514ae5ad40fef44c8a18dac021c5f15a8c4515d
jonpas <jonpas33@gmail.com>
2022-12-24 03:25:18 +0100

    Load userconfig in mission script macros

--- addons/mission/script_macros_mission.hpp
+++ addons/mission/script_macros_mission.hpp
@@ -1,16 +1,9 @@
-// Mission
+// Mission and AAR
 #define MISSION_TYPES ["Contract", "Non-Contract", "Training", "Special", "PvP", "Gimmick"]
 #define AUTOAAR_TYPES [0, 1, 2, 3, 4]
 #define AUTOAAR_PLAYER_COUNT 5
 
-// Statics
-#define ADMINS [ \
-    "76561198048995566", /* Jonpas */ \
-    "76561197985332763", /* rg */ \
-    "76561198085500182", /* Mike */ \
-    "76561198024182729", /* Laird */ \
-    "76561198324654204", /* gilleedoo */ \
-    "76561198040879834", /* JoramD */ \
-    "76561198062152747", /* Ethan */ \
-    "76561198021011658"  /* Zakharov */ \
-]
+// Userconfig
+#if __has_include("\userconfig\tac\staff.hpp")
+#include "userconfig\tac\staff.hpp"
+#endif
