24c2c3cf515318f6ff11aa47720d1fefc57c7b51
jonpas <jonpas33@gmail.com>
2022-12-24 04:02:07 +0100

    Move userconfig load and sync to mission (not binarized)

--- addons/mission/script_macros_mission.hpp
+++ addons/mission/script_macros_mission.hpp
@@ -2,8 +2,3 @@
 #define MISSION_TYPES ["Contract", "Non-Contract", "Training", "Special", "PvP", "Gimmick"]
 #define AUTOAAR_TYPES [0, 1, 2, 3, 4]
 #define AUTOAAR_PLAYER_COUNT 5
-
-// Userconfig
-#if __has_include("\userconfig\tac\staff.hpp")
-    #include "userconfig\tac\staff.hpp"
-#endif
