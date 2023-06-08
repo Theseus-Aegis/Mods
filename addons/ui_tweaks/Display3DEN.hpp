5911a1e8c29ea9b2de553a6658ef92cc91187017
Mike-MF <TyroneMF@hotmail.com>
2023-06-08 15:24:59 +0100

    UI Tweaks - Collapse categories by default in Eden (#587)

---
+++ addons/ui_tweaks/Display3DEN.hpp
@@ -0,0 +1,49 @@
+class ctrlTree;
+class ctrlControlsGroupNoScrollbars;
+class Display3DEN {
+    class Controls {
+        class PanelRight: ctrlControlsGroupNoScrollbars {
+            class Controls {
+                class PanelRightCreate: ctrlControlsGroupNoScrollbars {
+                    class Controls {
+                        class Create: ctrlControlsGroupNoScrollbars {
+                            class Controls {
+                                class CreateObjectWEST: ctrlTree {
+                                    defaultItem[] = {};
+                                };
+                                class CreateObjectEAST: CreateObjectWEST {
+                                    defaultItem[] = {};
+                                };
+                                class CreateObjectGUER: CreateObjectWEST {
+                                    defaultItem[] = {};
+                                };
+                                class CreateObjectCIV: CreateObjectWEST {
+                                    defaultItem[] = {};
+                                };
+                                class CreateObjectEMPTY;
+                                class CreateGroupWEST: CreateObjectEMPTY {
+                                    defaultItem[] = {};
+                                };
+                                class CreateGroupEAST: CreateObjectEMPTY {
+                                    defaultItem[] = {};
+                                };
+                                class CreateGroupGUER: CreateObjectEMPTY {
+                                    defaultItem[] = {};
+                                };
+                                class CreateGroupCIV: CreateObjectEMPTY {
+                                    defaultItem[] = {};
+                                };
+                                class CreateObjectLogic: CreateObjectEMPTY {
+                                    defaultItem[] = {};
+                                };
+                                class CreateMarkerIcon: CreateObjectEMPTY {
+                                    defaultItem[] = {};
+                                };
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+};
