abdd1aa97f77525d5c6b32c089f1388dbc65eb70
jonpas <jonpas33@gmail.com>
2023-06-13 21:23:32 +0200

    MELB Tweaks - Disable old Radar HUD element

--- optionals/melb_tweaks/RscInGameUI.hpp
+++ optionals/melb_tweaks/RscInGameUI.hpp
@@ -1,16 +1,18 @@
 class RscInGameUI {
     class RscUnitInfo;
     class Rsc_MELB_Turret_UnitInfo: RscUnitInfo {
-        onLoad = QUOTE([ARR_4('onLoad',_this,'RscUnitInfo','IGUI')] call (uiNamespace getVariable 'BIS_fnc_initDisplay'); _this call FUNC(handleFLIR));
+        onLoad = QUOTE([ARR_4('onLoad',_this,'RscUnitInfo','IGUI')] call (uiNamespace getVariable 'BIS_fnc_initDisplay'); _this call FUNC(handleFLIR)); // switched to custom function
     };
 
     class RscUnitInfoAir_NoRadar_MELB;
     class Rsc_MELB_UnitInfo: RscUnitInfoAir_NoRadar_MELB {
-        onLoad = QUOTE([ARR_4('onLoad',_this,'RscUnitInfo','IGUI')] call (uiNamespace getVariable 'BIS_fnc_initDisplay'); _this call FUNC(loadMFD));
+        controls[] = {"CA_BackgroundVehicle", "CA_BackgroundVehicleTitle", "CA_BackgroundVehicleTitleDark", "CA_BackgroundFuel", "CA_Vehicle", "CA_VehicleRole", "CA_HitZones", "CA_VehicleTogglesBackground", "CA_VehicleToggles", "CA_SpeedBackground", "CA_SpeedUnits", "CA_Speed", "CA_ValueFuel", "CA_AltBackground", "CA_AltUnits", "CA_Alt", "WeaponInfoControlsGroupRight"}; // removed "CA_Radar"
+        onLoad = QUOTE([ARR_4('onLoad',_this,'RscUnitInfo','IGUI')] call (uiNamespace getVariable 'BIS_fnc_initDisplay'); _this call FUNC(loadMFD)); // switched to custom function
     };
 
     class RscUnitInfoAirRTD_NoRadar_MELB;
     class Rsc_MELB_RTD_UnitInfo: RscUnitInfoAirRTD_NoRadar_MELB {
-        onLoad = QUOTE([ARR_4('onLoad',_this,'RscUnitInfoAirRTD_NoRadar_MELB','IGUI')] call (uiNamespace getVariable 'BIS_fnc_initDisplay'); _this call FUNC(loadMFD));
+        controls[] = {"CA_BackgroundVehicle", "CA_BackgroundVehicleTitle", "CA_BackgroundVehicleTitleDark", "CA_BackgroundFuel", "CA_Vehicle", "CA_VehicleRole", "CA_HitZones", "CA_VehicleTogglesBackground", "CA_VehicleToggles", "CA_SpeedBackground", "CA_SpeedUnits", "CA_Speed", "CA_ValueFuel", "CA_AltBackground", "CA_AltUnits", "CA_Alt", "CA_Horizon_Lite", "CA_Speed_Analogue", "CA_Altitude_Analogue", "CA_Horizon_Analogue", "CA_Stability_Analogue", "CA_Compass_Analogue", "WeaponInfoControlsGroupRight"}; // removed "CA_Radar"
+        onLoad = QUOTE([ARR_4('onLoad',_this,'RscUnitInfoAirRTD_NoRadar_MELB','IGUI')] call (uiNamespace getVariable 'BIS_fnc_initDisplay'); _this call FUNC(loadMFD)); // switched to custom function
     };
 };
