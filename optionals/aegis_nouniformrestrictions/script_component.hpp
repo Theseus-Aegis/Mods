8ede9309032d088edbc688074d454054b9145fda
jonpas <jonpas33@gmail.com>
2023-06-15 04:47:17 +0200

    Optionals - Add Aegis No Uniform Restirctions

---
+++ optionals/aegis_nouniformrestrictions/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT aegis_nouniformrestrictions
+#define COMPONENT_BEAUTIFIED Aegis No Uniform Restrictions
+#include "\x\tac\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_AEGIS_NOUNIFORMRESTRICTIONS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_AEGIS_NOUNIFORMRESTRICTIONS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_AEGIS_NOUNIFORMRESTRICTIONS
+#endif
+
+#include "\x\tac\addons\main\script_macros.hpp"
