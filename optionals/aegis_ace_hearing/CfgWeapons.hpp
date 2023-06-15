fc5bc31d9d356fb3d1bceaafaaf810881bd9a524
Mike-MF <TyroneMF@hotmail.com>
2023-06-15 03:07:54 +0100

    Optionals - Add Aegis ACE Hearing (#593)

---
+++ optionals/aegis_ace_hearing/CfgWeapons.hpp
@@ -0,0 +1,71 @@
+#define MACRO_HEARING \
+        ace_hearing_protection = 0.75; \
+        ace_hearing_lowerVolume = 0; \
+
+class CfgWeapons {
+    class H_HelmetI_I_01_cover_base_F;
+    class H_HelmetI_I_01_cover_F: H_HelmetI_I_01_cover_base_F {
+        MACRO_HEARING
+    };
+
+    class H_HelmetI_I_01_base_F;
+    class H_HelmetI_I_01_F: H_HelmetI_I_01_base_F {
+        MACRO_HEARING
+    };
+
+    class H_HelmetPunisher_base_F;
+    class H_HelmetPunisher_cover_green_F: H_HelmetPunisher_base_F {
+        MACRO_HEARING
+    };
+    class H_HelmetPunisher_headset_green_F: H_HelmetPunisher_base_F {
+        MACRO_HEARING
+    };
+
+    class H_HelmetSpecter_headset_base_F;
+    class H_HelmetSpecter_black_headset_F: H_HelmetSpecter_headset_base_F {
+        MACRO_HEARING
+    };
+    class H_HelmetSpecter_brown_headset_F: H_HelmetSpecter_headset_base_F {
+        MACRO_HEARING
+    };
+    class H_HelmetSpecter_headset_F: H_HelmetSpecter_headset_base_F {
+        MACRO_HEARING
+    };
+    class H_HelmetSpecter_paint_headset_F: H_HelmetSpecter_headset_base_F {
+        MACRO_HEARING
+    };
+
+    class H_HelmetSpecter_cover_base_F;
+    class H_HelmetSpecter_cover_khaki_F: H_HelmetSpecter_cover_base_F {
+        MACRO_HEARING
+    };
+    class H_HelmetSpecter_cover_taiga_F: H_HelmetSpecter_cover_base_F {
+        MACRO_HEARING
+    };
+    class H_HelmetSpecter_cover_UNO_F: H_HelmetSpecter_cover_base_F {
+        MACRO_HEARING
+    };
+    class rev_H_HelmetSpecter_cover_whex_CO: H_HelmetSpecter_cover_base_F {
+        MACRO_HEARING
+    };
+
+    class H_MK7_Base_F;
+    class H_MK7_AAF_F: H_MK7_Base_F {
+        MACRO_HEARING
+    };
+    class H_MK7_atacsfg_F: H_MK7_Base_F {
+        MACRO_HEARING
+    };
+    class H_MK7_Marar_F: H_MK7_Base_F {
+        MACRO_HEARING
+    };
+    class H_MK7_oli_F: H_MK7_Base_F {
+        MACRO_HEARING
+    };
+    class H_MK7_sand_F: H_MK7_Base_F {
+        MACRO_HEARING
+    };
+    class H_MK7_UN_F: H_MK7_Base_F {
+        MACRO_HEARING
+    };
+};
---
+++ optionals/aegis_ace_hearing/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT aegis_ace_hearing
+#define COMPONENT_BEAUTIFIED Aegis ACE3 Hearing
+#include "\x\tac\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_AEGIS_ACE_HEARING
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_AEGIS_ACE_HEARING
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_AEGIS_ACE_HEARING
+#endif
+
+#include "\x\tac\addons\main\script_macros.hpp"
