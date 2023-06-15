3a9e789a0ebcd214f6960554b921ae423aa4a606
Mike-MF <TyroneMF@hotmail.com>
2023-06-15 02:44:19 +0100

    Initial push

---
+++ optionals/aegis_ace_hearing/config.cpp
@@ -0,0 +1,24 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "tac_main",
+            "ace_hearing",
+            "A3_Aegis_Characters_F_Aegis_Headgear",
+            "A3_athena_Characters_F_athena_Headgear",
+            "A3_Atlas_Characters_F_Atlas_Headgear",
+            "A3_revolucion_Characters_F_revolucion_Headgear"
+        };
+        author = ECSTRING(main,Author);
+        authors[] = {"Mike"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+        skipWhenMissingDependencies = 1;
+    };
+};
+
+#include "CfgWeapons.hpp"
