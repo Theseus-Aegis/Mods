ccd03483730a36d4baf96d25a2cf9e6529da2e51
Mike-MF <TyroneMF@hotmail.com>
2023-06-18 23:37:12 +0100

    Delete Optional Compat Amp

--- optionals/compat_amp/CfgVehicles.hpp
+++
@@ -1,9 +0,0 @@
-class CfgVehicles {
-    class Box_AAF_Uniforms_F;
-
-    class EGVAR(supplies,Engineer): Box_AAF_Uniforms_F {
-        class TransportMagazines {
-            MACRO_ADDMAGAZINE(AMP_Breaching_Charge_Mag,12);
-        };
-    };
-};
