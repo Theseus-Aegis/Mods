6ef35ad49dba50244810f51b23148acb03242227
jonpas <jonpas33@gmail.com>
2023-06-19 00:16:10 +0200

    MELB Tweaks - Rewrite scripts and Add GEOLOCK (#592)

--- optionals/melb_ace_fastroping/CfgVehicles.hpp
+++
@@ -1,7 +0,0 @@
-class CfgVehicles {
-    class MELB_base;
-    class MELB_MH6M: MELB_base {
-        ace_fastroping_enabled = 1;
-        ace_fastroping_ropeOrigins[] = {{1.166, 0.79, -0.01}, {-1.166, 0.79, -0.01}};
-    };
-};
---
+++ optionals/melb_tweaks/XEH_PREP.hpp
@@ -0,0 +1,9 @@
+PREP(canToggleMFD);
+PREP(geolock);
+PREP(handleFLIR);
+PREP(loadMFD);
+PREP(setMFDMode);
+PREP(setMFDZoom);
+PREP(setupMFD);
+PREP(toggleMFD);
+PREP(unloadMFD);
