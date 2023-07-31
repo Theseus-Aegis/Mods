07b10c0a218cd704f8f47599b821dec227192e9f
Mike-MF <TyroneMF@hotmail.com>
2023-07-31 15:52:45 +0100

    ACRE

--- addons/a3_acre_racks/config.cpp
+++ addons/a3_acre_racks/config.cpp
@@ -6,11 +6,17 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"tac_main", "A3_Soft_F_Exp_LSV_01", "A3_Soft_F_Exp_LSV_02", "acre_sys_rack"};
+        requiredAddons[] = {
+            "tac_main",
+            "A3_Soft_F_Exp_LSV_01",
+            "A3_Soft_F_Exp_LSV_02",
+            "acre_sys_rack"
+        };
         author = ECSTRING(main,Author);
         authors[] = {"JoramD"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
+        skipWhenMissingDependencies = 1;
     };
 };
 
