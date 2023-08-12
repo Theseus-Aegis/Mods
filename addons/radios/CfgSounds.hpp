84772f08bdb0e4a05078fb47698eb54df6214590
JoramD0 <j.davids@hotmail.nl>
2023-08-13 00:24:18 +0200

    requested changes

--- addons/radios/CfgSounds.hpp
+++ addons/radios/CfgSounds.hpp
@@ -4,17 +4,17 @@
         name = QUOTE(var2); \
         sound[] = {QPATHTOF(sounds\var1.ogg), 1, 1, 10}; \
         titles[] = {}; \
-    };
+    }
 
 class CfgSounds {
-    SOUND(track1,90s_Boombap)
-    SOUND(track2,Midnight_Racer)
-    SOUND(track3,She_hopes_Im_hurting)
-    SOUND(track4,Smoke_Havana)
-    SOUND(track5,The_Slayer)
-    SOUND(track6,Eddie_the_Priest)
-    SOUND(track7,Saturday_Nights_Alright_for_Dying)
-    SOUND(track8,Bouquet_of_Coffee_Flavors)
-    SOUND(track9,Its_a_Lifestyle)
-    SOUND(track10,Icon_of_Sin)
+    SOUND(track1,90s_Boombap);
+    SOUND(track2,Midnight_Racer);
+    SOUND(track3,She_hopes_Im_hurting);
+    SOUND(track4,Smoke_Havana);
+    SOUND(track5,The_Slayer);
+    SOUND(track6,Eddie_the_Priest);
+    SOUND(track7,Saturday_Nights_Alright_for_Dying);
+    SOUND(track8,Bouquet_of_Coffee_Flavors);
+    SOUND(track9,Its_a_Lifestyle);
+    SOUND(track10,Icon_of_Sin);
 };
