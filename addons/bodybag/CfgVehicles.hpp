02cb9d3d7845f8ae34e43e2b1abec952c74f12d0
Mike-MF <TyroneMF@hotmail.com>
2023-10-26 20:35:12 +0100

    Update addons/bodybag/CfgVehicles.hpp
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- addons/bodybag/CfgVehicles.hpp
+++ addons/bodybag/CfgVehicles.hpp
@@ -2,6 +2,9 @@ class CfgVehicles {
     class ACE_bodyBagObject_old;
     class GVAR(Object_OrangeLogo): ACE_bodyBagObject_old {
         displayName = CSTRING(Orange_DisplayName);
+        // Needs new texture when we swap to ACE_bodyBagObject (new)
+        hiddenSelectionsTextures[] = {QPATHTOF(apl-sa\bodybag_co.paa)};
+
         maximumLoad = 0;
         transportMaxWeapons = 1;
         transportMaxMagazines = 1;
@@ -9,7 +12,5 @@ class CfgVehicles {
         ace_dragging_canCarry = 1; // 1 = enabled (0 disabled)
         ace_dragging_carryPosition[] = {0, 0.5, 1.2};  // XYZ offset, 0.5m ahead, 1.2m up.
         ace_dragging_carryDirection = 90; // Direction
-        // Needs new texture when we swap to ACE_bodyBagObject (new)
-        hiddenSelectionsTextures[] = {QPATHTOF(apl-sa\bodybag_co.paa)};
     };
 };
