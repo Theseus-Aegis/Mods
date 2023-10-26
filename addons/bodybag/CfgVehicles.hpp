8391ca4b364cf115b24fc720cab3b874aef7146c
Mike-MF <TyroneMF@hotmail.com>
2023-10-26 20:26:34 +0100

    Update addons/bodybag/CfgVehicles.hpp
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- addons/bodybag/CfgVehicles.hpp
+++ addons/bodybag/CfgVehicles.hpp
@@ -9,7 +9,7 @@ class CfgVehicles {
         ace_dragging_canCarry = 1; // 1 = enabled (0 disabled)
         ace_dragging_carryPosition[] = {0, 0.5, 1.2};  // XYZ offset, 0.5m ahead, 1.2m up.
         ace_dragging_carryDirection = 90; // Direction
-        // Needs new texture, if not done in time will just be black for now.
-        //hiddenSelectionsTextures[] = {QPATHTOF(apl-sa\bodybag_co.paa)};
+        // Needs new texture when we swap to ACE_bodyBagObject (new)
+        hiddenSelectionsTextures[] = {QPATHTOF(apl-sa\bodybag_co.paa)};
     };
 };
