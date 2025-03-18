b16b865ffee3ce0b10d0de7a5e4c9ff570884e8b
Mike-MF <TyroneMF@hotmail.com>
2025-03-18 10:57:17 +0000

    General - Apply suggestions from HEMTT 1.14.7 (#729)
    Suggestions from HEMTT 1.14.7

--- addons/ratelmarker/markerMenu.hpp
+++ addons/ratelmarker/markerMenu.hpp
@@ -1,7 +1,3 @@
-#define COORDINATE_X 0.385
-#define COORDINATE_Y 0.75
-#define COORDINATE_W 0.08
-
 class RscBackground;
 class RscText;
 class RscEdit;
@@ -14,38 +10,38 @@ class GVAR(menu) {
     onUnload = QUOTE(uiNamespace setVariable [ARR_2(QQGVAR(menuDisplay),nil)]);
     class controlsBackground {
         class Background: RscBackground {
-            x = QUOTE(COORDINATE_X);
-            y = QUOTE(COORDINATE_Y - 0.01);
-            h = QUOTE(0.11);
-            w = QUOTE(COORDINATE_W * 2 + 0.07);
+            x = 0.385;
+            y = 0.74;
+            h = 0.11;
+            w = 0.23;
             colorBackground[] = {0, 0, 0, 0.3};
         };
     };
     class controls {
         class X_Label: RscText {
             text = "X:";
-            x = QUOTE(COORDINATE_X);
-            y = QUOTE(COORDINATE_Y);
+            x = 0.385;
+            y = 0.75;
         };
         class X_Input: RscEdit {
             idc = GUI_ID_INPUT_X;
-            x = QUOTE(COORDINATE_X + 0.03);
-            y = QUOTE(COORDINATE_Y);
-            w = QUOTE(COORDINATE_W);
+            x = 0.415;
+            y = 0.75;
+            w = 0.08;
             colorBackground[] = {0, 0, 0, 0.5};
             tooltip = "Enter X Coordinate";
             tooltipColorBox[] = {0, 0, 0, 0.65};
         };
         class Y_Label: RscText {
             text = "Y:";
-            x = QUOTE(COORDINATE_X + 0.11);
-            y = QUOTE(COORDINATE_Y);
+            x = 0.495;
+            y = 0.75;
         };
         class Y_Input: RscEdit {
             idc = GUI_ID_INPUT_Y;
-            x = QUOTE(COORDINATE_X + 0.14);
-            y = QUOTE(COORDINATE_Y);
-            w = QUOTE(COORDINATE_W);
+            x = 0.525;
+            y = 0.75;
+            w = 0.08;
             colorBackground[] = {0, 0, 0, 0.5};
             tooltip = "Enter Y Coordinate";
             tooltipColorBox[] = {0, 0, 0, 0.65};
@@ -54,9 +50,9 @@ class GVAR(menu) {
             idc = -1;
             text = "Create Marker";
             onButtonClick = QUOTE(_this call FUNC(prepareMarker));
-            x = QUOTE(COORDINATE_X + 0.03);
-            y = QUOTE(COORDINATE_Y + 0.05);
-            w = QUOTE(COORDINATE_W + 0.09);
+            x = 0.415;
+            y = 0.8;
+            w = 0.17;
         };
     };
 };
