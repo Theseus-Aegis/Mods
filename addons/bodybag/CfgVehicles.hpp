0c50dca585d08fdd47a83f135b300b97aaabbaf6
Mike-MF <29175040+Mike-MF@users.noreply.github.com>
2025-10-13 10:47:01 -0500

    Bodybag - Make all carryable and add storage space (#746)

--- addons/bodybag/CfgVehicles.hpp
+++ addons/bodybag/CfgVehicles.hpp
@@ -1,15 +1,18 @@
 class CfgVehicles {
-    class ACE_bodyBagObject;
-    class GVAR(Object_OrangeLogo): ACE_bodyBagObject {
-        displayName = CSTRING(OrangeLogo_DisplayName);
-        hiddenSelectionsTextures[] = {QPATHTOF(data\bodybag_orange_co.paa)};
 
-        maximumLoad = 0;
-        transportMaxWeapons = 1;
-        transportMaxMagazines = 1;
-        transportMaxItems = 1;
+    class MapBoard_altis_F;
+    class ACE_bodyBagObject: MapBoard_altis_F {
         ace_dragging_canCarry = 1; // 1 = enabled (0 disabled)
         ace_dragging_carryPosition[] = {0, 0.5, 1.2};  // XYZ offset, 0.5m ahead, 1.2m up.
         ace_dragging_carryDirection = 90; // Direction
+        maximumLoad = 2000;
+        transportMaxWeapons = 1;
+        transportMaxMagazines = 1;
+        transportMaxItems = 1;
+    };
+
+    class GVAR(Object_OrangeLogo): ACE_bodyBagObject {
+        displayName = CSTRING(OrangeLogo_DisplayName);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\bodybag_orange_co.paa)};
     };
 };
